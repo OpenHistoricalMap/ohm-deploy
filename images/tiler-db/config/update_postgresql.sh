12a0f131a3fc18c7d6f2bef0a114e45108a7e0d5
Rub21 <rub2106@gmail.com>
2021-05-06 12:20:25 -0500

    Fix MAX_CONNECTIONS -> POSTGRES_DB_MAX_CONNECTIONS

--- images/tiler-db/config/update_postgresql.sh
+++ images/tiler-db/config/update_postgresql.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-if [[ "${MAX_CONNECTIONS}X" != "X" ]]; then
-  sed -i -e"s/^.*max_connections =.*$/max_connections = $MAX_CONNECTIONS/" $PGDATA/postgresql.conf
+if [[ "${POSTGRES_DB_MAX_CONNECTIONS}X" != "X" ]]; then
+  sed -i -e"s/^.*max_connections =.*$/max_connections = $POSTGRES_DB_MAX_CONNECTIONS/" $PGDATA/postgresql.conf
 fi
