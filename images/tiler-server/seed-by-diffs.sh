81d991467d57322f80936904d7ab90a957d08b11
Rub21 <rub2106@gmail.com>
2021-05-26 17:01:31 -0500

    Fix reading the latest expired files from s3

--- images/tiler-server/seed-by-diffs.sh
+++ images/tiler-server/seed-by-diffs.sh
@@ -16,24 +16,22 @@ completed_jobs=$workDir/imposm/completed.list
 completed_dir=$workDir/imposm/imposm3_expire_purged
 mkdir -p $completed_dir
 
-
 # List files in expire_dir
-imp_list=`find $expire_dir -name '*.tiles' -type f`
+imp_list=$(find $expire_dir -name '*.tiles' -type f)
 
 for f in $imp_list; do
-    echo "$f" >> $queued_jobs
+    echo "$f" >>$queued_jobs
 done
 
 # Sort the files and set unique rows
-if [ -f $queued_jobs ] ; then
-    sort -u $queued_jobs > $workDir/imposm/tmp.list && mv $workDir/imposm/tmp.list $queued_jobs
+if [ -f $queued_jobs ]; then
+    sort -u $queued_jobs >$workDir/imposm/tmp.list && mv $workDir/imposm/tmp.list $queued_jobs
 fi
 
 for f in $imp_list; do
     echo "seeding from $f"
     # Read each line on the tiles file
-    while IFS= read -r tile
-    do
+    while IFS= read -r tile; do
         bounds="$(python3 tile2bounds.py $tile)"
         # Get tiles values
         arraytile=($(echo "$tile" | tr '/' '\n'))
@@ -41,35 +39,36 @@ for f in $imp_list; do
         x=${arraytile[1]}
         y=${arraytile[2]}
 
-        # Check if cache  tile files are in s3
-        if [ "$TILER_CACHE_TYPE" == "s3" ] && [ ! -z "$zoom" ] &&  [ ! -z "$x" ]; then
-            set -x;
+        # if TILER_CACHE_TYPE=s3, let's remove the tiles from s3, it is faster than tegola cache purge
+        if [ "$TILER_CACHE_TYPE" == "s3" ] && [ ! -z "$zoom" ] && [ ! -z "$x" ]; then
+            set -x
             aws s3 rm --recursive s3://${TILER_CACHE_BUCKET}${TILER_CACHE_BASEPATH}/osm/$zoom/$x/
-            set +x;
+            set +x
         fi
 
+        # if TILER_CACHE_TYPE=file, use tegola cache purge
         if [ "$TILER_CACHE_TYPE" == "file" ]; then
-            set -x;
+            set -x
             tegola cache purge \
                 --config=/opt/tegola_config/config.toml \
-                --min-zoom=$minZoom \
+                --min-zoom=$zoom \
                 --max-zoom=20 \
                 --overwrite=true \
                 --bounds=$bounds \
                 tile-name=$tile
             err=$?
-            set +x;
+            set +x
             if [[ $err != "0" ]]; then
                 echo "tegola exited with error code $err"
                 exit
             fi
         fi
-    done < "$f"
-    echo "$f" >> $completed_jobs
+    done <"$f"
+    echo "$f" >>$completed_jobs
     mv $f $completed_dir
 done
 
-if [ -f $queued_jobs ] ; then
+if [ -f $queued_jobs ]; then
     echo "finished seeding"
     rm $queued_jobs
 fi
