962e24315e9989736ce08ce824378c25e87b9d24
Rub21 <ruben@developmentseed.org>
2021-09-10 14:20:43 -0500

    Add New Relic configuration

--- images/web/start.sh
+++ images/web/start.sh
@@ -11,6 +11,25 @@ production:
   password: ${POSTGRES_PASSWORD}
   encoding: utf8" > $workdir/config/database.yml
 
+echo "
+common: &default_settings
+  license_key: ${NEW_RELIC_LICENSE_KEY}
+  app_name: ${NEW_RELIC_APP_NAME}
+  distributed_tracing:
+    enabled: true
+  log_level: info
+development:
+  <<: *default_settings
+  app_name: ${NEW_RELIC_APP_NAME} (Development)
+test:
+  <<: *default_settings
+  monitor_mode: false
+staging:
+  <<: *default_settings
+  app_name: ${NEW_RELIC_APP_NAME} (Staging)
+production:
+  <<: *default_settings" > $workdir/config/newrelic.yml
+
 # Setting up the SERVER_URL and SERVER_PROTOCOL
 sed -i -e 's/server_url: "localhost"/server_url: "'$SERVER_URL'"/g' $workdir/config/application.yml
 sed -i -e 's/server_protocol: "http"/server_protocol: "'$SERVER_PROTOCOL'"/g' $workdir/config/application.yml
