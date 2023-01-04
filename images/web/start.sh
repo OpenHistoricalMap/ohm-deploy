755589001d1034d4e1dd2629ed4206b9cdd2c1b7
Rub21 <ruben@developmentseed.org>
2023-01-04 13:23:46 -0500

    Replace tokens from env vars

--- images/web/start.sh
+++ images/web/start.sh
@@ -47,6 +47,15 @@ sed -i -e 's/overpass-api.de/'$OVERPASS_URL'/g' $workdir/config/settings.yml
 sed -i -e 's/overpass-api.de/'$OVERPASS_URL'/g' $workdir/app/views/site/export.html.erb
 sed -i -e 's/overpass-api.de/'$OVERPASS_URL'/g' $workdir/app/assets/javascripts/index/export.js
 
+### REPLACE KEYS for iD
+DEV_CLIENT_ID="0tmNTmd0Jo1dQp4AUmMBLtGiD9YpMuXzHefitcuVStc"
+DEV_KEY="BTlNrNxIPitHdL4sP2clHw5KLoee9aKkA7dQbc0Bj7Q"
+files=$(find ./*/assets/ -type f -exec grep -l "$DEV_CLIENT_ID" {} \;)
+for f in $files; do
+  sed -i -e 's/'$DEV_CLIENT_ID'/'$OAUTH_CLIENT_ID'/g' $f
+  sed -i -e 's/'$DEV_KEY'/'$OAUTH_KEY'/g' $f
+done
+
 #### CHECK IF DB IS ALREADY UP AND START THE APP
 flag=true
 while "$flag" = true; do
@@ -59,7 +68,7 @@ while "$flag" = true; do
   done &
 
   # Enable assets:precompile, to take lates changes for assets in $workdir/config/settings.yml.
-  time bundle exec rake assets:precompile
+  time bundle exec rake i18n:js:export assets:precompile
 
   bundle exec rails db:migrate
   # Start the delayed jobs queue worker and  Start the app
