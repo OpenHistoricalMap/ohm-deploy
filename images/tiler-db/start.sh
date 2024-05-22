c6d4429709517f487b204a7c56b89d62f371b2cc
Rub21 <rub2106@gmail.com>
2024-05-22 14:03:24 -0500

    Add postgres config files - tiler

---
+++ images/tiler-db/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ENVIRONMENT=${ENVIRONMENT:-production}
+CONFIG_FILE=/etc/postgresql/postgresql.${ENVIRONMENT}.conf
+LOG_STATEMENT=${LOG_STATEMENT:-none}
+echo "Starting PostgreSQL using ${CONFIG_FILE}"
+# # Verificar permisos antes de intentar iniciar PostgreSQL
+# if [ ! -r "${CONFIG_FILE}" ]; then
+#     echo "Error: Cannot read configuration file ${CONFIG_FILE}"
+#     exit 1
+# fi
+exec su postgres -c "postgres -c config_file=${CONFIG_FILE} -c log_statement=${LOG_STATEMENT}"
