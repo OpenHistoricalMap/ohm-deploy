68571cdce2537bd18a68a3c24df05fd698c842d0
Rub21 <rub2106@gmail.com>
2024-05-24 15:10:35 -0500

    Update db-tiler config

--- images/tiler-db/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ENVIRONMENT=${ENVIRONMENT:-production}
-CONFIG_FILE=/etc/postgresql/postgresql.${ENVIRONMENT}.conf
-LOG_STATEMENT=${LOG_STATEMENT:-none}
-echo "Starting PostgreSQL using ${CONFIG_FILE}"
-exec su postgres -c "postgres -c config_file=${CONFIG_FILE} -c log_statement=${LOG_STATEMENT}"
