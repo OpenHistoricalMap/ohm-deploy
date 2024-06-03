2b52cfd523354abf8f5e3ed15886f95ffc13ade4
Rub21 <rub2106@gmail.com>
2024-06-03 13:42:40 -0500

    Update script for styles

--- images/web/update_map_styles.py
+++ images/web/update_map_styles.py
@@ -6,27 +6,22 @@ SERVER_URL = os.getenv("SERVER_URL", "www.openhistoricalmap.org")
 environment = "staging" if "staging" in SERVER_URL else "production"
 files = {
     "ohmVectorStyles.Original = ": {
-        "map-styles": "map-styles/main/main.json",
-        "ohm-website": "app/assets/javascripts/ohm.style.original.js",
-        "spritesheet_dir": "map-styles/main",
+        "map-styles": "/var/www/public/map-styles/main/main.json",
+        "ohm-website": "/var/www/app/assets/javascripts/ohm.style.original.js",
     },
     "ohmVectorStyles.Railway = ": {
-        "map-styles": "map-styles/rail/rail.json",
-        "ohm-website": "app/assets/javascripts/ohm.style.railway.js",
-        "spritesheet_dir": "map-styles/rail",
+        "map-styles": "/var/www/public/map-styles/rail/rail.json",
+        "ohm-website": "/var/www/app/assets/javascripts/ohm.style.railway.js",
     },
     "ohmVectorStyles.Woodblock = ": {
-        "map-styles": "map-styles/woodblock/woodblock.json",
-        "ohm-website": "app/assets/javascripts/ohm.style.woodblock.js",
-        "spritesheet_dir": "map-styles/woodblock",
+        "map-styles": "/var/www/public/map-styles/woodblock/woodblock.json",
+        "ohm-website": "/var/www/app/assets/javascripts/ohm.style.woodblock.js",
     },
     "ohmVectorStyles.JapaneseScroll = ": {
-        "map-styles": "map-styles/japanese_scroll/ohm-japanese-scroll-map.json",
-        "ohm-website": "app/assets/javascripts/ohm.style.japanese.js",
-        "spritesheet_dir": "map-styles/japanese_scroll",
+        "map-styles": "/var/www/public/map-styles/japanese_scroll/ohm-japanese-scroll-map.json",
+        "ohm-website": "/var/www/app/assets/javascripts/ohm.style.japanese.js",
     },
 }
-
 
 # Read json data from map-styles
 def read_json_file(file_path):
@@ -47,16 +42,6 @@ def write_json_file(js_file_path, key, json_data):
         print(f"Error updating {js_file_path}: {e}")
 
 
-# Copy directory to public/styles/
-def copy_directory(src_dir, dest_dir):
-    try:
-        if os.path.exists(dest_dir):
-            shutil.rmtree(dest_dir)
-        shutil.copytree(src_dir, dest_dir)
-    except Exception as e:
-        print(f"Error copying directory {src_dir} to {dest_dir}: {e}")
-
-
 # Loop through files
 for key, value in files.items():
     if "map-styles" in value:
@@ -73,18 +58,14 @@ for key, value in files.items():
                         "vtiles.openhistoricalmap.org",
                     )
                     .replace(
-                        "openhistoricalmap.github.io/map-styles/ohm_timeslider_tegola/ohm_spritezero_spritesheet",
-                        "openhistoricalmap.github.io/map-styles/ohm_timeslider_tegola/ohm_spritezero_spritesheet-production",
-                    )
-                    .replace(
-                        "openhistoricalmap.github.io/map-styles",
-                        "www.openhistoricalmap.org/styles",
+                        "openhistoricalmap.github.io",
+                        "www.openhistoricalmap.org",
                     )
                 )
             else:
                 json_str = json_str.replace(
-                    "openhistoricalmap.github.io/map-styles",
-                    "staging.openhistoricalmap.org/styles",
+                    "openhistoricalmap.github.io",
+                    "staging.openhistoricalmap.org",
                 )
 
             json_data = json.loads(json_str)
@@ -92,11 +73,4 @@ for key, value in files.items():
             ohm_website_path = value["ohm-website"]
             # Overwrite style file
             write_json_file(ohm_website_path, key, json_data)
-            
-            # Copy spritesheet to public dir
-            spritesheet_src_dir = value["spritesheet_dir"]
-            spritesheet_dest_dir = os.path.join(
-                "public", "styles", os.path.basename(spritesheet_src_dir)
-            )
-            copy_directory(spritesheet_src_dir, spritesheet_dest_dir)
             print(f"Updated map-style: {ohm_website_path}")
