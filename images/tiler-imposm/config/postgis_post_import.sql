ab6170d712ec65b641490f92ae1448426982a35b
Rub21 <rub2106@gmail.com>
2024-06-30 11:42:43 -0500

    Update function for date validation

--- images/tiler-imposm/config/postgis_post_import.sql
+++ images/tiler-imposm/config/postgis_post_import.sql
@@ -43,10 +43,29 @@ EXECUTE FUNCTION update_has_label_row();
 -- Create a function to validate dates
 
 CREATE OR REPLACE FUNCTION is_date_valid(date_text text) RETURNS boolean AS $$
+DECLARE
+    tmp_date date;
 BEGIN
-    PERFORM date_text::date;
-    RETURN TRUE;
-EXCEPTION WHEN others THEN
+    -- Try the format YYYY-MM-DD
+    BEGIN
+        tmp_date := to_date(date_text, 'YYYY-MM-DD');
+        RETURN TRUE;
+    EXCEPTION WHEN others THEN
+    END;
+
+    -- Try the format YYYY-MM
+    BEGIN
+        tmp_date := to_date(date_text || '-01', 'YYYY-MM-DD');
+        RETURN TRUE;
+    EXCEPTION WHEN others THEN
+    END;
+
+    -- Try the format YYYY
+    BEGIN
+        tmp_date := to_date(date_text || '-01-01', 'YYYY-MM-DD');
+        RETURN TRUE;
+    EXCEPTION WHEN others THEN
+    END;
     RETURN FALSE;
 END;
 $$ LANGUAGE plpgsql;
