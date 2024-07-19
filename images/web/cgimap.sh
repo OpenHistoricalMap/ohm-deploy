393b43d44f483253c1bb4384e5ef5352c34195c9
Rub21 <rub2106@gmail.com>
2024-07-19 15:23:32 -0500

    Update cgimap config

--- images/web/cgimap.sh
+++ images/web/cgimap.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+export PGPASSWORD=$POSTGRES_PASSWORD
 export CGIMAP_HOST=$POSTGRES_HOST
 export CGIMAP_DBNAME=$POSTGRES_DB
 export CGIMAP_USERNAME=$POSTGRES_USER
@@ -18,12 +20,15 @@ export CGIMAP_MAX_RELATION_MEMBERS="32000"
 # export CGIMAP_MODERATOR_MAXDEBT="1280"
 # export CGIMAP_PIDFILE="/var/www/cgimap.pid"
 
-# Check for web site status
 if [[ "$WEBSITE_STATUS" == "database_readonly" || "$WEBSITE_STATUS" == "api_readonly" ]]; then
   export CGIMAP_DISABLE_API_WRITE="true"
 fi
+
 if [[ "$WEBSITE_STATUS" == "database_offline" || "$WEBSITE_STATUS" == "api_offline" ]]; then
-    echo "Website is $WEBSITE_STATUS. No action required for cgimap service."
+  echo "Website is $WEBSITE_STATUS. No action required for cgimap service."
 else
+  export PGOPTIONS="-c enable_mergejoin=off -c enable_hashjoin=off"
+  psql -h $POSTGRES_HOST -U $POSTGRES_USER -c "SHOW enable_mergejoin;"
+  psql -h $POSTGRES_HOST -U $POSTGRES_USER -c "SHOW enable_hashjoin;"
   /usr/local/bin/openstreetmap-cgimap --port=8000 --daemon --instances=10
 fi
