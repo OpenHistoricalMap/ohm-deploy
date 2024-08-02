1729a549179341f3f1b00c27a8d0c6798ae20171
Rub21 <rub2106@gmail.com>
2024-08-02 14:22:40 -0500

    Add temporal statement_timeout

--- images/tiler-server/start.sh
+++ images/tiler-server/start.sh
@@ -6,7 +6,10 @@ while "$flag" = true; do
   pg_isready -h $POSTGRES_HOST -U $POSTGRES_USER -p $POSTGRES_PORT >/dev/null 2>&1 || continue
   flag=false
   echo "Running ANALYZE for tables"
-  time psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB -c "DO \$\$ DECLARE r RECORD; BEGIN FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP EXECUTE 'ANALYZE ' || r.tablename; END LOOP; END \$\$;"
+
+  # Set temporary statement_timeout
+  time psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB -c "SET statement_timeout = '300000'; DO \$\$ DECLARE r RECORD; BEGIN FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP EXECUTE 'ANALYZE ' || r.tablename; END LOOP; END \$\$;"
+
   export TEGOLA_SQL_DEBUG=LAYER_SQL:EXECUTE_SQL
   tegola serve --config=/opt/tegola_config/config.toml
 done
