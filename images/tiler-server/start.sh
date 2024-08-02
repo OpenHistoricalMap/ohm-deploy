616ede9dff2bf00ee156c9a6383b5adf93245e7a
Rub21 <rub2106@gmail.com>
2024-08-02 13:35:52 -0500

    Run ANALYZE to update stats for all tables - tiler db

--- images/tiler-server/start.sh
+++ images/tiler-server/start.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+export PGPASSWORD=$POSTGRES_PASSWORD
+
 flag=true
 while "$flag" = true; do
-  pg_isready -h $POSTGRES_HOST -U $POSTGRES_USER -p $POSTGRES_PORT >/dev/null 2>&2 || continue
+  pg_isready -h $POSTGRES_HOST -U $POSTGRES_USER -p $POSTGRES_PORT >/dev/null 2>&1 || continue
   flag=false
+  echo "Running ANALYZE for tables"
+  time psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB -c "DO \$\$ DECLARE r RECORD; BEGIN FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP EXECUTE 'ANALYZE ' || r.tablename; END LOOP; END \$\$;"
   export TEGOLA_SQL_DEBUG=LAYER_SQL:EXECUTE_SQL
   tegola serve --config=/opt/tegola_config/config.toml
 done
