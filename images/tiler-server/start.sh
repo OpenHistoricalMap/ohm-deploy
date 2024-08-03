18596ebe80ad2dab9c5563c02116a33e66d2a1c1
Rub21 <rub2106@gmail.com>
2024-08-02 22:32:23 -0500

    Update tiler start up

--- images/tiler-server/start.sh
+++ images/tiler-server/start.sh
@@ -5,9 +5,9 @@ flag=true
 while "$flag" = true; do
   pg_isready -h $POSTGRES_HOST -U $POSTGRES_USER -p $POSTGRES_PORT >/dev/null 2>&1 || continue
   flag=false
-  echo "Running ANALYZE for tables"
+  echo "Running VACUUM and ANALYZE for tables"
 
   # Set temporary statement_timeout
-  time psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB -c "SET statement_timeout = '300000'; DO \$\$ DECLARE r RECORD; BEGIN FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP EXECUTE 'ANALYZE ' || r.tablename; END LOOP; END \$\$;"
+  time psql -U $POSTGRES_USER -h $POSTGRES_HOST -d $POSTGRES_DB -c "SET statement_timeout = '300000'; DO \$\$ DECLARE r RECORD; BEGIN FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP EXECUTE 'VACUUM ' || r.tablename; EXECUTE 'ANALYZE ' || r.tablename; END LOOP; END \$\$;"
   TEGOLA_SQL_DEBUG=LAYER_SQL:EXECUTE_SQL tegola serve --config=/opt/tegola_config/config.toml
 done
