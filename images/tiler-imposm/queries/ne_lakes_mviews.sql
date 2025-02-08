c3e703aa129217a075f4be661f35ae3c01988ce1
Rub21 <rub2106@gmail.com>
2025-02-07 23:26:40 -0500

    Fix materiallized view fir NE lakes

--- images/tiler-imposm/queries/ne_lakes_mviews.sql
+++ images/tiler-imposm/queries/ne_lakes_mviews.sql
@@ -1,7 +1,10 @@
 -- This is an SQL script to merge lakes tables into materialized views.
+DROP MATERIALIZED VIEW IF EXISTS mview_ne_lakes CASCADE;
+
+-- Create a new materialized view
 CREATE MATERIALIZED VIEW mview_ne_lakes AS
 SELECT 
-    'ne_10m_lakes_' || ogc_fid AS ogc_fid,
+    'ne_10m_lakes_' || CAST(ogc_fid AS TEXT) AS ogc_fid,
     name,
     wkb_geometry,
     'ne_10m_lakes' AS source_table
@@ -9,7 +12,7 @@ FROM ne_10m_lakes
 
 UNION ALL
 SELECT 
-    'ne_10m_lakes_europe_' || ogc_fid AS ogc_fid,
+    'ne_10m_lakes_europe_' || CAST(ogc_fid AS TEXT) AS ogc_fid,
     name,
     wkb_geometry,
     'ne_10m_lakes_europe' AS source_table
@@ -17,11 +20,11 @@ FROM ne_10m_lakes_europe
 
 UNION ALL
 SELECT 
-    'ne_10m_lakes_north_america_' || ogc_fid AS ogc_fid,
+    'ne_10m_lakes_north_america_' || CAST(ogc_fid AS TEXT) AS ogc_fid,
     name,
     wkb_geometry,
     'ne_10m_lakes_north_america' AS source_table
-FROM ne_10m_lakes_north_america
+FROM ne_10m_lakes_north_america;
 
 CREATE INDEX idx_mview_ne_lakes_geom ON mview_ne_lakes USING GIST (wkb_geometry);
 CREATE INDEX idx_mview_ne_lakes_ogc_fid ON mview_ne_lakes (ogc_fid);
