92cb56e07698b4c02e9c47f6227cd86fd9fc18c2
Rub21 <rub2106@gmail.com>
2025-04-03 11:31:07 -0500

    Add mviews for other areas and update scripts

--- images/tiler-imposm/refresh_mviews.sh
+++ images/tiler-imposm/refresh_mviews.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# Update materialized views every 10 seconds. Actually, it will be in the queue until the previous one is done.
-SLEEP_INTERVAL=10
+# This script refreshes materialized views in PostgreSQL concurrently.
+# It is designed to run in the background and refresh views in parallel.
 PG_CONNECTION="postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB"
-
 function log_message() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
 }
 
 function refresh_mviews_group() {
     local group_name="$1"
-    shift
+    local sleep_interval="$2"
+    shift 2
     local materialized_views=("$@")
 
     while true; do
@@ -21,7 +21,7 @@ function refresh_mviews_group() {
                 log_message "[$group_name] ERROR refreshing $mview!"
             fi
         done
-        sleep "$SLEEP_INTERVAL"
+        sleep "$sleep_interval"
     done
 }
 
@@ -46,8 +46,10 @@ admin_views=(
 )
 
 transport_views=(
-    "mview_transport_lines_z5_7"
-    "mview_transport_lines_z8_9"
+    "mview_transport_lines_z6"
+    "mview_transport_lines_z7"
+    "mview_transport_lines_z8"
+    "mview_transport_lines_z9"
     "mview_transport_lines_z10_11"
     "mview_transport_lines_z12_13"
     "mview_transport_lines_z14_20"
@@ -72,13 +74,27 @@ landuse_views=(
     "mview_landuse_areas_centroid_z8_9"
     "mview_landuse_areas_centroid_z10_12"
     "mview_landuse_areas_centroid_z13_15"
-    "mview_landuse_areas_z3_5_subdivided"
-    "mview_landuse_areas_z6_7_subdivided"
-    "mview_landuse_areas_z8_9_subdivided"
 )
 
-# Start refreshing in parallel
-refresh_mviews_group "transport" "${transport_views[@]}" &
-refresh_mviews_group "admin" "${admin_views[@]}" &
-refresh_mviews_group "water" "${water_views[@]}" &
-refresh_mviews_group "landuse" "${landuse_views[@]}" &
+other_areas_views=(
+    "mview_other_areas_centroids_z6_8"
+    "mview_other_areas_centroids_z9_11"
+    "mview_other_areas_centroids_z12_14"
+    "mview_other_areas_centroids_z15_20"
+    "mview_other_areas_z6_8"
+    "mview_other_areas_z9_11"
+    "mview_other_areas_z12_14"
+)
+
+
+# Start refreshing in parallel with a sleep interval  all of them in average of 4 min refresh
+## Benchmark admin refresh those views takes 4 min to complete
+refresh_mviews_group "admin" 1 "${admin_views[@]}" &
+## Benchmark transport refresh those views takes 40 seconds to complete
+refresh_mviews_group "transport" 200 "${transport_views[@]}" &
+## Benchmark water areas and centroids refresh those views takes 1:20 min to complete
+refresh_mviews_group "water" 160 "${water_views[@]}" &
+## Benchmark landuse centroids refresh those views takes 22 secs to complete
+refresh_mviews_group "landuse" 220 "${landuse_views[@]}" &
+## Benchmark other areas and centroids refresh those views takes 3 secs to complete
+refresh_mviews_group "other_areas" 230 "${other_areas_views[@]}" &
