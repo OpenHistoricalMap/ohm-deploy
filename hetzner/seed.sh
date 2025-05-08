21a635a4e86d90c647e019d3d527109ba4d99eaf
Rub21 <rub2106@gmail.com>
2025-05-08 14:38:15 -0500

    Update docker images for tiler-server

--- hetzner/seed.sh
+++ hetzner/seed.sh
@@ -1,5 +1,54 @@
 #!/bin/bash
 
+UTILS_DIR="/opt/utils"
+CONFIG_DIR="/opt/config"
+TEGOLA_CONFIG_DIR="/opt/tegola_config"
+
+TAGINFO_URL="https://taginfo.openhistoricalmap.org/api/4/keys/all"
+LANGUAGE_SQL_FILE="${CONFIG_DIR}/languages.sql"
+TEGOLA_CONFIG_FILE="${TEGOLA_CONFIG_DIR}/config.toml"
+
+mkdir -p ${CONFIG_DIR} ${TEGOLA_CONFIG_DIR}
+
+# Extract language tags
+echo "Extracting languages from Taginfo..."
+python "${UTILS_DIR}/extract_taginfo_languages.py" \
+  --url "${TAGINFO_URL}" \
+  --output "${LANGUAGE_SQL_FILE}"
+
+# Build Tegola config
+echo "Building Tegola config..."
+python "${UTILS_DIR}/build_config.py" \
+  --output="${TEGOLA_CONFIG_FILE}" \
+  --provider_names "admin_boundaries_lines,
+admin_boundaries.centroids,
+admin_boundaries_maritime,
+place_areas,
+place_points,
+water_areas,
+water_areas.centroids,
+water_lines,
+transport_areas,
+transport_associated_streets,
+transport_lines,
+transport_points,
+route_lines,
+amenity_areas,
+amenity_areas.centroids,
+amenity_points,
+buildings,
+buildings_points_centroids,
+buildings.centroids,
+buildings_points,
+landuse_areas,
+landuse_areas.centroids,
+landuse_lines,
+landuse_points,
+other_areas,
+other_areas.centroids,
+other_lines,
+other_points"
+
 seed_global() {
   while true; do
     echo "Starting global seeding..."
@@ -28,7 +77,7 @@ seed_global() {
         --overwrite=true
     done
     echo "Global seeding completed. Sleeping for 3 hour..."
-    sleep 10800
+    sleep 600
   done
 }
 
