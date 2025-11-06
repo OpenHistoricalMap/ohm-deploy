929cb9f4d15dfd0e9122544750fec3f9075a8f00
Rub21 <rub2106@gmail.com>
2025-11-06 11:31:35 -0500

    Update transport and other areas

--- images/tiler-imposm/queries/ohm_mviews/others.sql
+++ images/tiler-imposm/queries/ohm_mviews/others.sql
@@ -1,42 +1,111 @@
--- ============================================================================
--- Zoom 8-9: Medium simplification (50m), medium areas (>1M m² = 1 km²)
--- ============================================================================
-SELECT create_areas_mview('osm_other_areas', 'mv_other_areas_z8_9', 50, 1000000, 'id, osm_id, type');
-SELECT create_points_centroids_mview('mv_other_areas_z8_9', 'mv_other_points_centroids_z8_9', NULL);
+-- Create materialized views for other areas with different simplification levels
+-- Using the generalized create_areas_mview function
 
 -- ============================================================================
--- Zoom 10-11: Medium-low simplification (15m), medium areas (>50K m² = 0.05 km²)
+-- Zoom 8-9:
+-- Medium simplification (50m)
+-- Medium areas (>1M m² = 1 km²)
 -- ============================================================================
-SELECT create_areas_mview('osm_other_areas', 'mv_other_areas_z10_11', 15, 50000, 'id, osm_id, type');
-SELECT create_points_centroids_mview('mv_other_areas_z10_11', 'mv_other_points_centroids_z10_11', NULL);
+SELECT create_areas_mview(
+    'osm_other_areas',
+    'mv_other_areas_z8_9',
+    50,
+    1000000,
+    'id, osm_id, type',
+    NULL
+);
+SELECT create_points_centroids_mview(
+    'mv_other_areas_z8_9',
+    'mv_other_points_centroids_z8_9',
+    NULL
+);
+
+-- ============================================================================
+-- Zoom 10-11:
+-- Medium-low simplification (15m)
+-- Medium areas (>50K m² = 0.05 km²)
+-- ============================================================================
+SELECT create_areas_mview(
+    'osm_other_areas',
+    'mv_other_areas_z10_11',
+    15,
+    50000,
+    'id, osm_id, type',
+    NULL
+);
+SELECT create_points_centroids_mview(
+    'mv_other_areas_z10_11',
+    'mv_other_points_centroids_z10_11',
+    NULL
+);
 
 -- ============================================================================
 -- Prepare points materialized view for higher zoom levels (12+)
 -- ============================================================================
 -- Prepare points table with necessary columns (start_decdate, end_decdate, area_m2, area_km2, etc.)
 -- This must be done before creating centroids views that include points
-SELECT create_points_mview('osm_other_points', 'mv_other_points');
+SELECT create_points_mview(
+    'osm_other_points',
+    'mv_other_points'
+);
 
 -- ============================================================================
--- Zoom 12-13: Low simplification (10m), small areas (>10K m² = 0.01 km²)
+-- Zoom 12-13:
+-- Low simplification (10m)
+-- Small areas (>10K m² = 0.01 km²)
+-- Include other points
 -- ============================================================================
-SELECT create_areas_mview('osm_other_areas', 'mv_other_areas_z12_13', 10, 10000, 'id, osm_id, type');
-SELECT create_points_centroids_mview('mv_other_areas_z12_13', 'mv_other_points_centroids_z12_13', 'mv_other_points');
+SELECT create_areas_mview(
+    'osm_other_areas',
+    'mv_other_areas_z12_13',
+    10,
+    10000,
+    'id, osm_id, type',
+    NULL
+);
+SELECT create_points_centroids_mview(
+    'mv_other_areas_z12_13',
+    'mv_other_points_centroids_z12_13',
+    'mv_other_points'
+);
 
 -- ============================================================================
--- Zoom 14-15: Very low simplification (5m), very small areas (>5K m² = 0.005 km²)
+-- Zoom 14-15:
+-- Very low simplification (5m)
+-- Very small areas (>5K m² = 0.005 km²)
+-- Include other points
 -- ============================================================================
-SELECT create_areas_mview('osm_other_areas', 'mv_other_areas_z14_15', 5, 5000, 'id, osm_id, type');
-SELECT create_points_centroids_mview('mv_other_areas_z14_15', 'mv_other_points_centroids_z14_15', 'mv_other_points');
+SELECT create_areas_mview(
+    'osm_other_areas',
+    'mv_other_areas_z14_15',
+    5,
+    5000,
+    'id, osm_id, type',
+    NULL
+);
+SELECT create_points_centroids_mview(
+    'mv_other_areas_z14_15',
+    'mv_other_points_centroids_z14_15',
+    'mv_other_points'
+);
 
 -- ============================================================================
--- Zoom 16-20: No simplification, all areas
+-- Zoom 16-20:
+-- No simplification
+-- All areas
+-- Include other points
 -- ============================================================================
-SELECT create_areas_mview('osm_other_areas', 'mv_other_areas_z16_20', 0, 0, 'id, osm_id, type');
-SELECT create_points_centroids_mview('mv_other_areas_z16_20', 'mv_other_points_centroids_z16_20', 'mv_other_points');
+SELECT create_areas_mview(
+    'osm_other_areas',
+    'mv_other_areas_z16_20',
+    0,
+    0,
+    'id, osm_id, type',
+    NULL
+);
+SELECT create_points_centroids_mview(
+    'mv_other_areas_z16_20',
+    'mv_other_points_centroids_z16_20',
+    'mv_other_points'
+);
 
-
--- -- ============================================================================
--- -- Create materialized views for landuse lines
--- -- ============================================================================
--- SELECT create_generic_mview( 'osm_landuse_lines', 'mv_landuse_lines_z14_20', ARRAY['osm_id', 'type', 'class']);
--- images/tiler-imposm/queries/ohm_mviews/transport_areas.sql
+++ images/tiler-imposm/queries/ohm_mviews/transport_areas.sql
@@ -1,104 +1,95 @@
 -- ============================================================================
--- Function: create_transport_areas_mview
--- Description:
---   Creates a materialized view combining:
---     - Centroids of named transport polygons from the table 'osm_transport_areas'.
---     - Includes multilingual name columns using the helper function get_language_columns().
---     - Allows optional filtering by the 'type' column.
---
--- Parameters:
---   view_name  TEXT                     - Name of the materialized view to be created.
---   types      TEXT[] DEFAULT ARRAY['*'] 
---              - Optional list of values to filter the 'type' column.
---              - If types = ARRAY['*'], no filtering is applied and all types are included.
---              - If types contains one or more values, a SQL filter "type = ANY (...)" is applied.
---
--- Notes:
---   - Drops any existing view using a temporary swap mechanism.
---   - Geometry is calculated as the centroid of the maximum inscribed circle.
---   - Includes spatial (GiST) index on geometry and a unique index on (osm_id, type, class).
---   - Supports temporal filtering via start_date, end_date, and corresponding decimal date fields.
---   - The resulting view is useful for generalization and rendering transport area centroids in vector tiles.
--- ============================================================================
-DROP FUNCTION IF EXISTS create_transport_areas_mview;
-
-CREATE OR REPLACE FUNCTION create_transport_areas_mview(
-    view_name TEXT,
-    types TEXT[] DEFAULT ARRAY['*']
-)
-RETURNS void AS $$
-DECLARE 
-    lang_columns TEXT := get_language_columns();
-    tmp_view_name TEXT := view_name || '_tmp';
-    unique_columns TEXT := 'osm_id, type, class';
-    sql_create TEXT;
-    type_filter_areas TEXT := 'TRUE';
-BEGIN
-   
-    IF NOT (array_length(types, 1) = 1 AND types[1] = '*') THEN
-        type_filter_areas := format('type = ANY (%L)', types);
-    END IF;
-
-    sql_create := format($sql$
-        CREATE MATERIALIZED VIEW %I AS
-        SELECT
-            geometry,
-            id,
-            ABS(osm_id) AS osm_id, 
-            NULLIF(name, '') AS name, 
-            class, 
-            type, 
-            NULLIF(start_date, '') AS start_date,
-            NULLIF(end_date, '') AS end_date,
-            isodatetodecimaldate(pad_date(start_date, 'start'), FALSE) AS start_decdate,
-            isodatetodecimaldate(pad_date(end_date, 'end'), FALSE) AS end_decdate,
-            area,
-            ROUND(CAST(area AS numeric), 1)::numeric(20,1) AS area_m2,
-            ROUND(CAST(area AS numeric) / 1000000, 1)::numeric(20,1) AS area_km2,
-            'polygon' as source, 
-            %s
-        FROM osm_transport_areas
-        WHERE %s
-            -- From https://github.com/OpenHistoricalMap/issues/issues/1194
-          AND NOT (class = 'highway' AND type IN ('motorway', 
-                                                'motorway_link', 
-                                                'trunk', 
-                                                'trunk_link', 
-                                                'primary', 
-                                                'primary_link', 
-                                                'secondary', 
-                                                'secondary_link', 
-                                                'tertiary', 
-                                                'tertiary_link', 
-                                                'unclassified', 
-                                                'residential', 
-                                                'service', 
-                                                'living_street', 
-                                                -- 'pedestrian', 
-                                                -- 'track', 
-                                                -- 'path', 
-                                                -- 'footway', 
-                                                'cycleway', 
-                                                'bridleway'
-                                                -- 'steps'
-                                                )
-                    );
-    $sql$, tmp_view_name, lang_columns, type_filter_areas);
-
-    PERFORM finalize_materialized_view(
-        tmp_view_name,
-        view_name,
-        unique_columns,
-        sql_create
-    );
-END;
-$$ LANGUAGE plpgsql;
-
--- ============================================================================
--- Create materialized views for transport areas
--- ============================================================================
+-- Zoom 10-11:
+-- Medium-low simplification (15m)
+-- Medium areas (>50K m² = 0.05 km²)
 -- We include aerodrome to start at zoom 10 from https://github.com/OpenHistoricalMap/issues/issues/1083
- -- From https://github.com/OpenHistoricalMap/issues/issues/1141 add 'apron', 'terminal' 
-SELECT create_transport_areas_mview('mv_transport_areas_z10_11', ARRAY['aerodrome', 'apron', 'terminal' ]);
+-- From https://github.com/OpenHistoricalMap/issues/issues/1141 add 'apron', 'terminal' 
+-- ============================================================================
+SELECT create_areas_mview(
+    'osm_transport_areas',
+    'mv_transport_areas_z10_11',
+    15,
+    50000,
+    'id, osm_id, type',
+    'type in (''aerodrome'', ''apron'', ''terminal'')'
+);
 
-SELECT create_transport_areas_mview('mv_transport_areas_z12_20', ARRAY['*']);
+SELECT create_points_centroids_mview(
+    'mv_transport_areas_z10_11',
+    'mv_transport_points_centroids_z10_11',
+    NULL
+);
+
+-- ============================================================================
+-- Prepare points materialized view for higher zoom levels (12+)
+-- ============================================================================
+-- Prepare points table with necessary columns (start_decdate, end_decdate, area_m2, area_km2, etc.)
+-- This must be done before creating centroids views that include points
+SELECT create_points_mview(
+    'osm_transport_points',
+    'mv_transport_points'
+);
+
+-- ============================================================================
+-- Zoom 12-13:
+-- Low simplification (10m)
+-- Small areas (>10K m² = 0.01 km²)
+-- Include other points
+-- Exclude closed highways from https://github.com/OpenHistoricalMap/issues/issues/1194
+-- ============================================================================
+SELECT create_areas_mview(
+    'osm_transport_areas',
+    'mv_transport_areas_z12_13',
+    10,
+    10000,
+    'id, osm_id, type',
+    'NOT (class = ''highway'' AND type IN (''motorway'', ''motorway_link'', ''trunk'', ''trunk_link'', ''primary'', ''primary_link'', ''secondary'', ''secondary_link'', ''tertiary'', ''tertiary_link'', ''unclassified'', ''residential'', ''service'', ''living_street'', ''cycleway'', ''bridleway''))'
+);
+SELECT create_points_centroids_mview(
+    'mv_transport_areas_z12_13',
+    'mv_transport_points_centroids_z12_13',
+    'mv_transport_points'
+);
+
+-- ============================================================================
+-- Zoom 14-15:
+-- Very low simplification (5m)
+-- Very small areas (>5K m² = 0.005 km²)
+-- Include transport points
+-- Exclude closed highways from https://github.com/OpenHistoricalMap/issues/issues/1194
+-- ============================================================================
+SELECT create_areas_mview(
+    'osm_transport_areas',
+    'mv_transport_areas_z14_15',
+    5,
+    5000,
+    'id, osm_id, type',
+    'NOT (class = ''highway'' AND type IN (''motorway'', ''motorway_link'', ''trunk'', ''trunk_link'', ''primary'', ''primary_link'', ''secondary'', ''secondary_link'', ''tertiary'', ''tertiary_link'', ''unclassified'', ''residential'', ''service'', ''living_street'', ''cycleway'', ''bridleway''))'
+);
+SELECT create_points_centroids_mview(
+    'mv_transport_areas_z14_15',
+    'mv_transport_points_centroids_z14_15',
+    'mv_transport_points'
+);
+
+-- ============================================================================
+-- Zoom 16-20:
+-- No simplification
+-- All areas
+-- Include transport points
+-- Exclude closed highways from https://github.com/OpenHistoricalMap/issues/issues/1194
+
+-- ============================================================================
+SELECT create_areas_mview(
+    'osm_transport_areas',
+    'mv_transport_areas_z16_20',
+    0,
+    0,
+    'id, osm_id, type',
+    'NOT (class = ''highway'' AND type IN (''motorway'', ''motorway_link'', ''trunk'', ''trunk_link'', ''primary'', ''primary_link'', ''secondary'', ''secondary_link'', ''tertiary'', ''tertiary_link'', ''unclassified'', ''residential'', ''service'', ''living_street'', ''cycleway'', ''bridleway''))'
+);
+SELECT create_points_centroids_mview(
+    'mv_transport_areas_z16_20',
+    'mv_transport_points_centroids_z16_20',
+    'mv_transport_points'
+);
