bc35a61f53ab4d8dfb721265b82295a80c577c55
Rub21 <rub2106@gmail.com>
2026-08-13 09:41:49 +0200

    Use THREADS env var for nominatim replication indexing

--- images/nominatim/update.sh
+++ images/nominatim/update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 # Because of crashing updates, we wrote this small script to update every minute the Nominatim api
 LOCAL_API=http://localhost:8080
+# Load container env saved by docker-entrypoint.sh (cron has a minimal env)
+[ -f /app/cron-env.sh ] && . /app/cron-env.sh
 echo "Updating nominatim api...."
 wget -q --spider $LOCAL_API
 if [ ! $? -ne 0 ]; then
-    sudo -u nominatim nominatim replication --project-dir /nominatim --catch-up >>/var/log/replication.log
+    sudo -u nominatim nominatim replication --project-dir /nominatim --catch-up --threads ${THREADS:-4} >>/var/log/replication.log
 fi
